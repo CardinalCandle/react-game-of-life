d9f88ddc0ab6e9692aace32f6f97380c74f14e9f
CardinalCandle <youssef-kj@outlook.fr>
2023-08-22 23:22:15 +0100

    removed files , added grid

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+
+class Box extends Component {
+  selectBox = () => {
+    this.props.selectBox(this.props.row, this.props.col)
+  }
+  render() {
+    return (
+      <div 
+      className={this.props.boxClass}
+      id={this.props.id}
+      onClick={this.selectBox}
+      />
+    );
+  }
+}
+
+
+class Grid extends Component {
+  render() {
+    const width = (this.props.cols * 16);
+    var rowsArr = []
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.cols; i++) {
+      for (var j = 0; j < this.props.rows; j++) {
+        let boxId = i + "_" + j;
+        boxClass = this.props.gridFull[i][j]? "box on" : "box off"
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        );
+      }
+    }
+    return (
+      <div className='grid' style={{width: width}}>
+      {rowsArr}
+      </div>
+    );
+  }
+}
+
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.speed = 100;
+    this.rows = 27;
+    this.cols = 27;
+    this.state = {
+      generation: 0,
+      gridFull: Array(this.rows).fill().map(() => 
+      Array(this.cols).fill(false))
+    }
+  }
+  render() {
+    return (
+      <div>
+      <h1>The Game of Life</h1>
+      <Grid
+        gridFull={this.state.gridFull}
+        rows={this.rows}
+        cols={this.cols}
+        selectBox={this.selectBox}
+      />
+      <h2>Generation : {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Main/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
